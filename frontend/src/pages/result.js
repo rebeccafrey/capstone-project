aa43ab5631af8ac1df7d9a39a7d02967daffc9eb
rebeccafrey <rebecca.frey101@gmail.com>
2020-05-12 18:00:47 +0200

    Add description text depending on result

--- frontend/src/pages/result.js
+++ frontend/src/pages/result.js
@@ -6,7 +6,7 @@ import Divider from '../ui/Divider'
 export default function Result() {
   return (
     <>
-      <MainStyled>
+      <main>
         <p>
           Hier findest du dein Ergebnis. Je mehr Aussagen du zugestimmt hast,
           desto höher ist die Wahrscheinlichkeit, dass du introvertiert bist.
@@ -14,17 +14,42 @@ export default function Result() {
           wahrscheinlich extravertierter.
         </p>
         <Divider />
-        <ScaleStyled />
-        <TopScalePoint>20 - eher introvertiert</TopScalePoint>
-        <ResultBubble>{countTotalCheckedBoxes()}</ResultBubble>
-        <BottomScalePoint>0 - eher extravertiert</BottomScalePoint>
-      </MainStyled>
+        <ResultStyled>
+          <ScaleStyled />
+          <TopScalePoint>20 - eher introvertiert</TopScalePoint>
+          <ResultBubble>{countTotalCheckedBoxes()}</ResultBubble>
+          <BottomScalePoint>0 - eher extravertiert</BottomScalePoint>
+        </ResultStyled>
+        <Divider />
+        <p>{showTextDependingOnResult()}</p>
+      </main>
     </>
   )
+  function showTextDependingOnResult() {
+    if (countTotalCheckedBoxes() >= 0 && countTotalCheckedBoxes() <= 6) {
+      return 'Nur wenige der Statements treffen auf dich zu. Du bist also eher extravertiert, aber was bedeutet das? Der Hauptunterschied zur Introvertiertheit besteht darin, dass du deine Batterien vor allem durch soziale Interaktion aufladen kannst. Treffen mit Freunden sind wichtig für dich, und du ziehst daraus Energie. Für Introverierte ist dies meist eher anstrengend, und sie benötigen Zeit für sich, um Kraft zu tanken.'
+    } else if (
+      countTotalCheckedBoxes() >= 7 &&
+      countTotalCheckedBoxes() <= 13
+    ) {
+      return 'Du pendelst dich in der Mitte ein. Die Tendenz zur Mitte ist bei vielen gegeben und zeigt, dass du Charakterzüge von beiden Ausprägungen in dir trägst - und du sehr wahrscheinlich mal ein wenig Zeit für dich brauchst, oder auch mal gern kraftgebende Zeit mit anderen, auch größeren Gruppen, verbringst.'
+    } else if (
+      countTotalCheckedBoxes() >= 14 &&
+      countTotalCheckedBoxes() <= 20
+    ) {
+      return 'Viele der Statements treffen auf dich zu. Du bist also eher introvertiert, aber was bedeutet das? Der Hauptunterschied zur Extravertiertheit besteht darin, dass du deine Batterien vor allem in der Zeit, die du ohne viel soziale Interaktion verbringst, wieder aufladen kannst. Treffen mit Freunden sind zwar auch wichtig für dich, können aber regelrechte Krafträuber darstellen. Extravertierte dagegen sind weniger gern allein - es verhält sich bei ihnen genau andersherum.'
+    } else {
+      return ''
+    }
+  }
 }
 
-const MainStyled = styled.main`
-  height: 100vh;
+const ResultStyled = styled.div`
+  display: grid;
+  grid-template-columns: 56px auto;
+  grid-template-rows: 20px 260px 20px;
+  column-gap: 8px;
+  margin-bottom: 24px;
 `
 const ScaleStyled = styled.div`
   width: 24px;
@@ -32,24 +57,26 @@ const ScaleStyled = styled.div`
   border-radius: 8px;
   background: var(--secondary-40);
   margin-left: 32px;
+  grid-column: 1;
+  grid-row: 1 / 3;
 `
 const TopScalePoint = styled.span`
   font-size: 20px;
   font-weight: 500;
   color: var(--secondary-40);
-  position: relative;
-  left: 60px;
-  bottom: 300px;
   width: 220px;
+  grid-column: 2;
+  grid-row: 1;
+  align-self: center;
 `
 const BottomScalePoint = styled.span`
   font-size: 20px;
   font-weight: 500;
   color: var(--secondary-40);
-  position: relative;
-  left: 60px;
-  bottom: 220px;
   width: 220px;
+  grid-column: 2;
+  grid-row: 3;
+  align-self: center;
 `
 const ResultBubble = styled.div`
   width: 156px;
@@ -61,7 +88,7 @@ const ResultBubble = styled.div`
   font-weight: 700;
   line-height: 156px;
   text-align: center;
-  position: relative;
-  left: 100px;
-  bottom: 260px;
+  grid-column: 2;
+  grid-row: 2/2;
+  place-self: center;
 `
